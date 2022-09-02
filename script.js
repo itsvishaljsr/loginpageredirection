f4057853977b071e8f29d0a17fd24f54dc97d5c6
Vishal Prajapati <vishalprajapatijsr@gmail.com>
2022-09-03 01:03:04 +0530

    functionality changes

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
+//function to validate user and password
 
 function loginValidation() {
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
-
+    console.log("working function!!!")
+    
     if(username == "vishal" && password == "1234") {
-        window.location.href = "https://stackoverflow.com/";
+        window.location.assign("home.html");
+        alert("Correct Username and Password :)");
+    }
+    else if (username != "" || password != "") {
+        window.location.assign("login.html");
+        alert("Invalid UserName and Password\nPlease enter correct Username and Password"); 
+        document.getElementById('username').value = null;
+        document.getElementById('password').value = null;
     }
     else {
-        alert("Invalid UserName and Password");
+        // window.location.assign("login.html");
+        alert("Please enter Username and Password"); 
     }
 }
